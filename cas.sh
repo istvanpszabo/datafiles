9364d750bc169bfb146f404a6e82afd3822c7ff1
istvanpszabo <istvan.p.szabo@gmail.com>
2015-11-14 19:50:19 +0100

    Update cas.sh

--- cas.sh
+++ cas.sh
@@ -1,1 +1,1 @@
-spark-shell --jars /home/cb/temp/spark-cassandra-connector-2.11-1.5.0-M2.jar --conf spark.cassandra.connection.host=localhost --driver-memory 2g --executor-memory 3g
+spark-shell --jars /home/cb/temp/spark-cassandra-connector_2.11-1.5.0-M2.jar --conf spark.cassandra.connection.host=localhost --driver-memory 2g --executor-memory 3g
