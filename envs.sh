902077e792dbaa5680beb53344db7c15cbc3c098
istvanpszabo <istvan.p.szabo@gmail.com>
2015-11-14 18:02:11 +0100

    Create envs.sh

---
+++ envs.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+export PATH="$PATH:/home/cb/temp/scala-2.11.4/bin"
+export PATH="$PATH:/home/cb/temp/spark-1.5.2-bin-hadoop2.6/bin"
+export PATH="$PATH:/home/cb/temp/spark-1.5.2-bin-hadoop2.6/sbin"
