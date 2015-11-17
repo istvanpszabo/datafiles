1b9f63472526edaf24de024a267d1e64df1597bd
Istvan Szabo <szabo@cleverbridge.com>
2015-11-17 20:10:28 +0100

    a

--- cas2.sh
+++
@@ -1,1 +0,0 @@
-spark-shell --packages com.datastax.spark:spark-cassandra-connector_2.11:1.5.0-M2
--- download.sh
+++
@@ -1,1 +0,0 @@
-wget http://repo1.maven.org/maven2/com/datastax/spark/spark-cassandra-connector_2.11/1.5.0-M2/spark-cassandra-connector_2.11-1.5.0-M2.jar
